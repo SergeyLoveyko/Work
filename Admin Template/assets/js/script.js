279d70fd2dc77adf713a606b369b1edbc26f8f5f
Sergey <loveykosergey@ukr.net>
2021-03-24 17:52:55 +0200

    add project Admin Template

---
+++ Admin Template/assets/js/script.js
@@ -0,0 +1,27 @@
+$('#btn-sidebar').on('click', function() {
+  $('#sidebar').toggleClass('active');
+});
+
+$('#btn-sidebar-hide').on('click', function() {
+  $('#sidebar').toggleClass('hide');
+});
+
+$('#btn-sidebar-color').on('click', function() {
+  $('#sidebar').toggleClass('color');
+});
+
+/* 
+$(ducument).ready(function() {
+
+  $('#btn-sidebar').on('click', function() {
+    // $('#sidebar').toggleClass('active');
+  });
+
+});
+ */
+/* 
+$( "#btn-sidebar" ).click(function() {
+  $('.collapse_main').pslideU(); 
+});
+ */
+
